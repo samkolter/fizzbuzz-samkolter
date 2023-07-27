a7f62f7dcac2d02f3ef28cffb9ead11544e7e674
Sam Kolter <110484668+samkolter@users.noreply.github.com>
2023-07-27 19:44:16 -0400

    Update fizzbuzz.py

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,2 +1,14 @@
-# add your code here
+for i in range(1, 101):
+# FizzBuzz
+ if i% 3 == 0 and i % 5 == 0:
+    print ("FizzBuzz")
+# Fizz
+ elif i % 3 == 0:
+    print ("Fizz")
+# Buzz
+ elif i % 5 == 0:
+    print ("Buzz")
+# None of the above
+ else: 
+    print (i)
 
